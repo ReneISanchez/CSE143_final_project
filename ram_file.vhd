f971831e1e62bb3633b210c85d3e15b4401ec62b
Rene Sanchez <risanche@ucsd.edu>
2016-03-10 13:09:04 -0800

    "Working" RAM

--- ram_file.vhd
+++ ram_file.vhd
@@ -12,36 +12,73 @@ port (
     ctrl_clk   : in  std_logic;
     ctrl_wr    : in  std_logic;
     ctrl_addr  : in  std_logic_vector(1 downto 0);
-    ctrl_din   : in  std_logic_vector(29491199 downto 0);
-    ctrl_dout  : out std_logic_vector(29491199 downto 0);
+    ctrl_din   : in  std_logic_vector(16588799 downto 0);
+    ctrl_dout  : out std_logic_vector(16588799 downto 0);
      
     -- Multiplier port
     mul_clk   : in  std_logic;
     mul_wr    : in  std_logic;
     mul_addr  : in  std_logic_vector(1 downto 0);
-    mul_din   : in  std_logic_vector(29491199 downto 0);
-    mul_dout  : out std_logic_vector(29491199 downto 0)
+    mul_din   : in  std_logic_vector(16588799 downto 0);
+    mul_dout  : out std_logic_vector(16588799 downto 0)
 );
 end ram_file;
  
 architecture behavioral of ram_file is
     -- Shared memory
-    type ram_t is array (11059199 downto 0) of std_logic_vector(7 downto 0);
+    type ram_t is array (8313599 downto 0) of std_logic_vector(7 downto 0);
 	 --type ram_t is std_logic_vector(0 to (2**30)*8);
     shared variable mem : ram_t;
 	 
-	 signal location_ctrl : std_logic_vector(58982399 downto 0);
-	 signal location_mul :  std_logic_vector(58982399 downto 0);
-	 constant mat : integer := 29491200;
+	 signal location_ctrl : std_logic_vector(33177599 downto 0);
+	 signal location_mul :  std_logic_vector(33177599 downto 0);
+	 constant mat : integer := 16588800;
+	 constant res : integer := 9331200;
+	 constant loc_size: integer := 33177600;
 	 
-	 function memWrite(loc: in std_logic_vector; ram_mem: in ram_t) return std_logic_vector is
-			variable output: std_logic_vector(29491199 downto 0) := std_logic_vector(to_unsigned(0,mat));
+	 function memGet(loc: in std_logic_vector; ram_mem: in ram_t; isRes: in integer) return std_logic_vector is
+			variable output: std_logic_vector(16588799 downto 0) := std_logic_vector(to_unsigned(0,mat));
 			variable count: integer := 0;
+			variable offset: integer := 16588800;
+			variable temp: std_logic_vector(7 downto 0) := "00000000";
+			variable outCount: integer := 7;
 		begin
-			for i in to_integer(unsigned(loc)) to (294921200 + to_integer(unsigned(loc))) loop
+			if(isRes = 1) then
+				offset := 9331200;
+			end if;
+		
+			for i in to_integer(unsigned(loc)) to (offset - 1 + to_integer(unsigned(loc))) loop
 				if(count = 7) then
-					output((i) downto (i-7)) := ram_mem((i/7) - 1);
+					--output((i) downto (i-7)) := ram_mem((i/7) - 1);
+					temp := ram_mem((i/7) - 1);
+					output((outCount) downto (outCount-7)) := temp;
 					count := 0;
+					outCount := outCount + 7;
+				else
+					count := count + 1;
+				end if;
+			end loop;
+				
+			return output;
+ 
+		end memGet;
+		
+	 function memWrite(loc: in std_logic_vector; ram_mem: in ram_t; data_in: in std_logic_vector; isRes: in integer) 
+		return ram_t is
+			variable output: ram_t := ram_mem;
+			variable count: integer := 0;
+			variable offset: integer := 16588800;
+			variable outCount: integer := 7;
+		begin
+			if(isRes = 1) then
+				offset := 9331200;
+			end if;
+		
+			for i in to_integer(unsigned(loc)) to (offset - 1 + to_integer(unsigned(loc))) loop
+				if(count = 7) then
+					output((i/7) - 1) := data_in(outCount downto (outCount-7));
+					count := 0;
+					outCount := outCount + 7;
 				else
 					count := count + 1;
 				end if;
@@ -50,24 +87,6 @@ architecture behavioral of ram_file is
 			return output;
  
 		end memWrite;
-		
-	 function memReplace(loc: in std_logic_vector; ram_mem: in ram_t; data_in: in std_logic_vector) 
-		return ram_t is
-			variable output: ram_t := ram_mem;
-			variable count: integer := 0;
-		begin
-			for i in to_integer(unsigned(loc)) to (294921200 + to_integer(unsigned(loc))) loop
-				if(count = 7) then
-					output((i/7) - 1) := data_in(i downto (i-7));
-					count := 0;
-				else
-					count := count + 1;
-				end if;
-			end loop;
-				
-			return output;
- 
-		end memReplace;
 begin
 
 -- location = A when addr is 00
@@ -78,9 +97,9 @@ begin
 process(ctrl_addr)
 begin
 	case ctrl_addr is
-		when "00" => location_ctrl <= std_logic_vector(to_unsigned(0, 58982400));			--Matrix A
-		when "01" => location_ctrl <= std_logic_vector(to_unsigned(mat, 58982400));		--Matrix B
-		when "10" => location_ctrl <= std_logic_vector(to_unsigned(mat*2, 58982400));		--Result
+		when "00" => location_ctrl <= std_logic_vector(to_unsigned(0, loc_size));			--Matrix A
+		when "01" => location_ctrl <= std_logic_vector(to_unsigned(mat, loc_size));		--Matrix B
+		when "10" => location_ctrl <= std_logic_vector(to_unsigned(mat*2, loc_size));		--Result
 		when others => null;
 	end case;
 end process;
@@ -89,9 +108,9 @@ end process;
 process(mul_addr)
 begin
 	case mul_addr is
-		when "00" => location_mul <= std_logic_vector(to_unsigned(0, 58982400));		--Matrix A
-		when "01" => location_mul <= std_logic_vector(to_unsigned(mat, 58982400));		--Matrix B
-		when "10" => location_mul <= std_logic_vector(to_unsigned(mat*2, 58982400));	--Result
+		when "00" => location_mul <= std_logic_vector(to_unsigned(0, loc_size));		--Matrix A
+		when "01" => location_mul <= std_logic_vector(to_unsigned(mat, loc_size));		--Matrix B
+		when "10" => location_mul <= std_logic_vector(to_unsigned(mat*2, loc_size));	--Result
 		when others => null;
 	end case;
 end process;
@@ -101,11 +120,17 @@ process(ctrl_clk)
 begin
     if(ctrl_clk'event and ctrl_clk='1') then
         if(ctrl_wr='0') then
-            --mem(to_integer(unsigned(location_ctrl)) to (3686400-1) + to_integer(unsigned(location_ctrl))) := ctrl_din((3686400-1));
-				mem := memReplace(location_ctrl, mem, ctrl_din);
+				if(ctrl_addr = "10") then
+					mem := memWrite(location_ctrl, mem, ctrl_din, 1);
+				else
+					mem := memWrite(location_ctrl, mem, ctrl_din, 0);
+				end if;
         else
-				--ctrl_dout <= mem(to_integer(unsigned(location_ctrl)) to to_integer(unsigned(location_ctrl)) + (3686400-1));
-				ctrl_dout <= memWrite(location_ctrl, mem);
+				if(ctrl_addr = "10") then
+					ctrl_dout <= memGet(location_ctrl, mem,1);
+				else 
+					ctrl_dout <= memGet(location_ctrl, mem,0);			
+				end if;
 			end if;
     end if;
 end process;
@@ -115,11 +140,17 @@ process(mul_clk)
 begin
     if(mul_clk'event and mul_clk='1') then
         if(mul_wr='0') then
-            --mem(to_integer(unsigned(location_mul)) to (3686400-1) + to_integer(unsigned(location_mul))) := mul_din((3686400-1));
-				mem := memReplace(location_mul, mem, mul_din);
+				if(mul_addr = "10") then
+					mem := memWrite(location_mul, mem, mul_din,1);
+				else	
+					mem := memWrite(location_mul, mem, mul_din,0);					
+				end if;
         else
-				--mul_dout <= mem(to_integer(unsigned(location_mul)) to to_integer(unsigned(location_mul)) + (3686400-1));
-				mul_dout <= memWrite(location_mul, mem);
+				if(mul_addr = "10") then
+					mul_dout <= memGet(location_mul, mem,1);
+				else
+					mul_dout <= memGet(location_mul, mem,0);
+				end if;
 			end if;
     end if;
 end process;
--- ram_tb.vhd
+++ ram_tb.vhd
@@ -15,15 +15,15 @@ ARCHITECTURE behavior OF ram_tb IS
     ctrl_clk   : in  std_logic;
     ctrl_wr    : in  std_logic;
     ctrl_addr  : in  std_logic_vector(1 downto 0);
-    ctrl_din   : in  std_logic_vector((3686400)*8 - 1 downto 0);
-    ctrl_dout  : out std_logic_vector((3686400)*8 - 1 downto 0);
+    ctrl_din   : in  std_logic_vector(16588799 downto 0);
+    ctrl_dout  : out std_logic_vector(16588799 downto 0);
      
     -- Multiplier port
     mul_clk   : in  std_logic;
     mul_wr    : in  std_logic;
     mul_addr  : in  std_logic_vector(1 downto 0);
-    mul_din   : in  std_logic_vector((3686400)*8 - 1 downto 0);
-    mul_dout  : out std_logic_vector((3686400)*8 - 1 downto 0)
+    mul_din   : in  std_logic_vector(16588799 downto 0);
+    mul_dout  : out std_logic_vector(16588799 downto 0)
         );
     END COMPONENT;
 	 
@@ -31,17 +31,17 @@ ARCHITECTURE behavior OF ram_tb IS
    signal ctrl_clk : std_logic := '0';
    signal ctrl_wr : std_logic := '1';
    signal ctrl_addr : std_logic_vector(1 downto 0) := "00";
-	signal ctrl_din : std_logic_vector((3686400)*8 - 1 downto 0) := std_logic_vector(to_unsigned(10, (3686400)*8));
-	signal ctrl_dout : std_logic_vector((3686400)*8 - 1 downto 0) := std_logic_vector(to_unsigned(0, (3686400)*8));
+	signal ctrl_din : std_logic_vector(16588799 downto 0) := std_logic_vector(to_unsigned(10, 16588800));
+	signal ctrl_dout : std_logic_vector(16588799 downto 0) := std_logic_vector(to_unsigned(0, 16588800));
 	
    --multiplier in/out
    signal mul_clk : std_logic := '0';
    signal mul_wr : std_logic := '1';
    signal mul_addr : std_logic_vector(1 downto 0) := "00";
-	signal mul_din : std_logic_vector((3686400)*8 - 1 downto 0) := std_logic_vector(to_unsigned(0, (3686400)*8));
-	signal mul_dout : std_logic_vector((3686400)*8 - 1 downto 0) := std_logic_vector(std_logic_vector(to_unsigned(0, (3686400)*8)));
+	signal mul_din : std_logic_vector(16588799 downto 0) := std_logic_vector(to_unsigned(0, 16588800));
+	signal mul_dout : std_logic_vector(16588799 downto 0) := std_logic_vector(std_logic_vector(to_unsigned(0, 16588800)));
 	
-   constant clk_period : time := 1 ns;
+   constant clk_period : time := 5 ps;
 BEGIN
     -- Instantiate the Unit Under Test (UUT)
    uut: ram_file PORT MAP (
@@ -65,7 +65,7 @@ BEGIN
 		  mul_clk <= '0';
         wait for clk_period/2;  --for 0.5 ns signal is '0'.
         ctrl_clk <= '1';
-		  mul_clk <= '0';
+		  mul_clk <= '1';
         wait for clk_period/2;  --for next 0.5 ns signal is '1'.
    end process;
 
@@ -77,7 +77,7 @@ BEGIN
 		mul_addr <= "10";
 		
       wait for 25 ps;
-		ctrl_din <= std_logic_vector(to_unsigned(50, (3686400)*8));
+		ctrl_din <= std_logic_vector(to_unsigned(50, 16588800));
 		ctrl_addr <= "01";
 		
       wait for 25 ps;
